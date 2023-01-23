139e73c05ef82968dee185609592f35bba7fad46
jensaja <asselmanjens@gmail.com>
2023-01-23 13:24:30 +0100

    Add helper methods for changes to quality and sellIn

--- Java/src/main/java/com/gildedtros/GildedTros.java
+++ Java/src/main/java/com/gildedtros/GildedTros.java
@@ -7,39 +7,47 @@ class GildedTros {
         this.items = items;
     }
 
+    private void decreaseQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality--;
+        }
+    }
+
+    private void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality++;
+        }
+    }
+
+    private void decreaseSellIn(Item item) {
+        item.sellIn--;
+    }
+
     public void updateQuality() {
         for (Item item : items) {
             if (!item.name.equals("Good Wine")
                     && !item.name.equals("Backstage passes for Re:Factor")
                     && !item.name.equals("Backstage passes for HAXX"))
             {
-                if (item.quality > 0) {
-                    if (!item.name.equals("B-DAWG Keychain")) {
-                        item.quality--;
-                    }
+                if (!item.name.equals("B-DAWG Keychain")) {
+                    decreaseQuality(item);
                 }
             } else {
-                if (item.quality < 50) {
-                    item.quality++;
+                increaseQuality(item);
 
-                    if (item.name.equals("Backstage passes for Re:Factor") || item.name.equals("Backstage passes for HAXX") ) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality++;
-                            }
-                        }
+                if (item.name.equals("Backstage passes for Re:Factor") || item.name.equals("Backstage passes for HAXX") ) {
+                    if (item.sellIn < 11) {
+                        increaseQuality(item);
+                    }
 
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality++;
-                            }
-                        }
+                    if (item.sellIn < 6) {
+                        increaseQuality(item);
                     }
                 }
             }
 
             if (!item.name.equals("B-DAWG Keychain")) {
-                item.sellIn--;
+                decreaseSellIn(item);
             }
 
             if (item.sellIn < 0) {
@@ -47,16 +55,14 @@ class GildedTros {
                     if (!item.name.equals("Backstage passes for Re:Factor") && !item.name.equals("Backstage passes for HAXX")) {
                         if (item.quality > 0) {
                             if (!item.name.equals("B-DAWG Keychain")) {
-                                item.quality--;
+                                decreaseQuality(item);
                             }
                         }
                     } else {
                         item.quality = 0;
                     }
                 } else {
-                    if (item.quality < 50) {
-                        item.quality++;
-                    }
+                    increaseQuality(item);
                 }
             }
         }
